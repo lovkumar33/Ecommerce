6b3cee2ef263885e862b411589c13c5cf66f8422
lovkumar33 <lovkumar33@gmail.com>
2021-04-16 23:03:32 +0530

    Add product

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -7,7 +7,7 @@
 <div class="col-sm-4">
   <div class="trending-wrapper">
     <h4>Result for Products</h4>
-    <a class="btn btn-success" href="ordernow">Order Now</a><br><br>
+    <br>
     @foreach ($products as $item)
     <div class="row searched-item cart-list-devider">
       <div class="col-sm-3">
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::get("removecart/{id}",[ProductController::class,'removeCart']);
 Route::get("ordernow",[ProductController::class,'orderNow']);
 Route::post("orderplace",[ProductController::class,'orderPlace']);
 Route::get("myorders",[ProductController::class,'myOrders']);
+Route::post("/addproduct",[ProductController::class,'addproduct']);
+Route::view("/addproduct","addproduct");
